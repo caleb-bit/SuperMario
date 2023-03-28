ff07573ad551842fa63301b1655cc758e26d47ae
caleb <calebshim248@gmail.com>
2023-03-28 13:08:28 -0400

    LevelPanel created

--- src/Frontend/LevelPanel.java
+++ src/Frontend/LevelPanel.java
@@ -1,6 +1,48 @@
 package Frontend;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class LevelPanel extends JPanel {
+abstract public class LevelPanel extends JPanel implements KeyListener {
+    private ArrayList<UIComponent> components;
+    private Player player;
+    private BackendManager backend;
+    private EventHandler eventHandler;
+    enum Direction {UP, DOWN, RIGHT, LEFT}
+
+    LevelPanel(BackendManager backend) {}
+
+    public void paintComponent(Graphics g) {
+        for (UIComponent component : components) {
+            component.paint(g);
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        eventHandler.keyPressed(e.getKeyCode());
+    }
+
+    public void keyReleased(KeyEvent e) {}
+
+    public void keyTyped(KeyEvent e){}
+
+    public ArrayList<UIComponent> getUIComponents() {
+        return components;
+    }
+    public Player getPlayer() {
+        return player;
+    }
+
+    public BackendManager getBackend() {
+        return backend;
+    }
+    public EventHandler getEventHandler() {
+        return eventHandler;
+    }
+
+
+
 }
