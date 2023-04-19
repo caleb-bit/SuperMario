78098f57d57b43d15c7d963fd50dca95fd0508eb
daowstin <austinzou2603@gmail.com>
2023-04-19 13:19:22 -0400

    started onkeypressed

--- src/Backend/Powerup.java
+++ src/Backend/Powerup.java
@@ -3,9 +3,13 @@ package Backend;
 public abstract class Powerup extends GameObject{
     boolean taken;
     int duration;
+    String name;
     Powerup(GamePosition position){
         super(position, 0, 0);
         taken = false;
+    }
+    public String getName(){
+        return name;
     }
     public boolean getTaken(){
         return taken;
