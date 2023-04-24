29cdfd8032eca253c7cddbb922b099f8461a0f36
caleb <calebshim248@gmail.com>
2023-04-24 12:26:41 -0400

    added enemies

---
+++ src/Backend/Piranha.java
@@ -0,0 +1,7 @@
+package Backend;
+
+public class Piranha extends Enemy {
+    Piranha(GamePosition position, int velX, int velY) {
+        super(position, velX, velY);
+    }
+}
---
+++ src/Frontend/UIKoopa.java
@@ -0,0 +1,26 @@
+package Frontend;
+
+import Backend.GameObject;
+
+import java.awt.*;
+
+public class UIKoopa extends UIEnemy{
+    UIKoopa(GameObject gameObject) {
+        super(gameObject);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+    }
+
+    @Override
+    public boolean onComponent(UIPosition pos) {
+        return false;
+    }
+
+    @Override
+    boolean correctGameObjectType(GameObject gameObject) {
+        return false;
+    }
+}
