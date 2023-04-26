b7c99d213f021d38889c179ef15e4df1f6770274
caleb <calebshim248@gmail.com>
2023-04-26 12:49:10 -0400

    uiflower draws at correct pos

--- src/Frontend/UIFlower.java
+++ src/Frontend/UIFlower.java
@@ -7,8 +7,8 @@ import Backend.Powerup;
 import java.awt.*;
 
 public class UIFlower extends UIPowerup {
-    int width = 50;
-    int height = 50;
+    int width = 30;
+    int height = 30;
 
     public UIFlower(GameObject gameObject) {
         super(gameObject);
@@ -16,18 +16,23 @@ public class UIFlower extends UIPowerup {
 
     @Override
     public void paint(Graphics g) {
+        //outer circle
         g.setColor(Color.red);
-        fillOval(g, getIntX(), getIntY(), width, height / 2);
+        fillOval(g, getIntX() + width * 3 / 5, getIntY()-height*3/4, width, height / 2);
+        //middle circle
         g.setColor(Color.yellow);
-        fillOval(g, getIntX(), getIntY(), width * 2 / 3, height / 3);
+        fillOval(g, getIntX() + width * 3 / 5, getIntY()-height*3/4, width * 2 / 3, height / 3 );
+        // inner circle
         g.setColor(Color.white);
-        fillOval(g, getIntX(), getIntY(), width / 3, height / 6);
+        fillOval(g, getIntX() + width * 3 / 5, getIntY()-height*3/4, width / 3, height / 6);
         g.setColor(Color.green);
-        g.fillRect(getIntX() - width / 10, getIntY() + height / 4, width / 5, height / 2);
-        g.fillPolygon(new int[]{getIntX(), getIntX() - width * 2 / 5, getIntX() - width * 3 / 5, getIntX() - width * 2 / 5, getIntX()},
-                new int[]{getIntY() + height / 2 + height / 4, getIntY() + height / 2 + height / 8, getIntY() - height / 8 + height / 2, getIntY() - height / 4 + height / 2, getIntY() + height / 4 + height / 2}, 5);
-        g.fillPolygon(new int[]{getIntX(), getIntX() + width * 2 / 5, getIntX() + width * 3 / 5, getIntX() + width * 2 / 5, getIntX()},
-                new int[]{getIntY() + height / 4 + height / 2, getIntY() + height / 8 + height / 2, getIntY() - height / 8 + height / 2, getIntY() - height / 4 + height / 2, getIntY() + height / 4 + height / 2}, 5);
+        //stem
+        g.fillRect(getIntX() - width / 10 + width * 3 / 5, getIntY() + height / 4 - height * 3 / 4, width / 5, height / 2);
+        //leaves
+        g.fillPolygon(new int[]{getIntX() + width * 3 / 5, getIntX() - width * 2 / 5 + width * 3 / 5, getIntX(), getIntX() - width * 2 / 5 + width * 3 / 5, getIntX() + width * 3 / 5},
+                new int[]{getIntY(), getIntY() + height / 2 + height / 8 - height * 3 / 4, getIntY() - height / 8 + height / 2 - height * 3 / 4, getIntY() - height / 4 + height / 2 - height * 3 / 4, getIntY()}, 5);
+        g.fillPolygon(new int[]{getIntX() + width * 3 / 5, getIntX() + width * 2 / 5 + width * 3 / 5, getIntX() + width * 3 / 5 + width * 3 / 5, getIntX() + width * 2 / 5 + width * 3 / 5, getIntX() + width * 3 / 5},
+                new int[]{getIntY(), getIntY() + height / 8 + height / 2 - height * 3 / 4, getIntY() - height / 8 + height / 2 - height * 3 / 4, getIntY() - height / 4 + height / 2 - height * 3 / 4, getIntY()}, 5);
     }
 
     public void fillOval(Graphics g, int centerX, int centerY, int width, int height) {
@@ -36,10 +41,10 @@ public class UIFlower extends UIPowerup {
 
     @Override
     public boolean onComponent(UIPosition pos) {
-        return getIntX() - width * 3 / 5 <= (int) pos.getX()
-                && (int) pos.getX() <= getIntX() + width * 3 / 5
-                && getIntY() - height / 4 <= getIntY()
-                && getIntY() <= getIntY() * 3 / 4;
+        return getIntX() <= (int) pos.getX()
+                && (int) pos.getX() <= getIntX() + width
+                && getIntY() - height <= (int) pos.getY()
+                && (int) pos.getY() <= getIntY();
     }
 
     @Override
