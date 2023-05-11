35bd4bd3d6ad641cf5b7801f7f0179fef8294cbf
daowstin <austinzou2603@gmail.com>
2023-05-11 18:18:21 -0400

    map 4 extension

--- src/Backend/Map4.java
+++ src/Backend/Map4.java
@@ -7,18 +7,27 @@ public class Map4 extends Map{
     Map4(){
         ArrayList<GameObject> gameObjects = new ArrayList<>();
         ArrayList<GamePosition> gamePositions = new ArrayList<>();
-        gameObjects.add(new Land(0,120,0,-5));
+        gameObjects.add(new Goomba(new GamePosition(90, 0)));
+        gameObjects.add(new Goomba(new GamePosition(91, 0)));
+        gameObjects.add(new Goomba(new GamePosition(92, 0)));
+        gameObjects.add(new Koopa(new GamePosition(97, 0)));
+        gameObjects.add(new Koopa(new GamePosition(98, 0)));
+        gameObjects.add(new Koopa(new GamePosition(99, 0)));
+        gameObjects.add(new Koopa(new GamePosition(105, 0)));
+        gameObjects.add(new Land(0,150,0,-5));
         gameObjects.add(new Yoshi(new GamePosition(19, 0)));
         gameObjects.add(new Cliff(new GamePosition(5, 0), 2));
         gameObjects.add(new Cliff(new GamePosition(16, 0), 2));
-        gameObjects.add(new Trap(new GamePosition(19, 3), 3));
+        gameObjects.add(new Trap(new GamePosition(20, 3), 3));
         gameObjects.add(new Cliff(new GamePosition(21, 0), 2));
+        gameObjects.add(new Cliff(new GamePosition(110, 0), 3));
         gameObjects.add(new Trap(new GamePosition(32, 3), 3));
         gameObjects.add(new Trap(new GamePosition(38, 3), 3));
         gameObjects.add(new Trap(new GamePosition(42, 3), 3));
         gameObjects.add(new Trap(new GamePosition(55, 0), 3));
         gameObjects.add(new Trap(new GamePosition(58, 0), 3));
         gameObjects.add(new Trap(new GamePosition(63, 3), 3));
+        gameObjects.add(new Trap(new GamePosition(80, 3), 3));
         gameObjects.add(new Cliff(new GamePosition(65, 0), 10));
         gameObjects.add(new Land(68, 70,3.5 ,3));
         gameObjects.add(new Land(72, 74,3.5 ,3));
@@ -29,8 +38,10 @@ public class Map4 extends Map{
         gameObjects.add(new Coin(new GamePosition(76, 3.5)));
         gamePositions.add(new GamePosition(23, 0));
         gamePositions.add(new GamePosition(85, 0));
+        gamePositions.add(new GamePosition(120, 0));
         gameObjects.add(new Flag(new GamePosition(23,0), false));
-        gameObjects.add(new Flag(new GamePosition(85,0),true));
+        gameObjects.add(new Flag(new GamePosition(85,0),false));
+        gameObjects.add(new Flag(new GamePosition(120, 0), true));
         initObjects(gameObjects, gamePositions);
     }
 }
