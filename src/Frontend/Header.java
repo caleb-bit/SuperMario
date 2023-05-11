2d6ea2e387103b3378a5941eb5591f171de0090c
daowstin <austinzou2603@gmail.com>
2023-05-10 23:41:06 -0400

    added coins to header

--- src/Frontend/Header.java
+++ src/Frontend/Header.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Header{
-    public static void drawHeader(BufferedImage image, int timeLeft, int lives, Powerup power){
+    public static void drawHeader(BufferedImage image, int timeLeft, int lives, int coins, Powerup power){
         Graphics2D graphics = (Graphics2D) image.getGraphics();
         graphics.setBackground(new Color(0,0,0,0));
         graphics.clearRect(0,0,720,30);
@@ -15,6 +15,7 @@ public class Header{
         graphics.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,18));
         graphics.drawString("Time Left: " + (timeLeft),10,22);
         graphics.drawString("Lives: "+ lives, 250,22);
-        graphics.drawString("Powerup: "+ power.getName(), 570,22);
+        graphics.drawString("Coins: " + coins, 400, 22);
+        graphics.drawString("Powerup: "+ (power == null ? "None": power.getName()), 570,22);
     }
 }
