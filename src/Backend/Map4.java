008bd4bed05d2bdc45999e2e2896b61f2925a620
daowstin <austinzou2603@gmail.com>
2023-05-11 01:01:02 -0400

    map edit

--- src/Backend/Map4.java
+++ src/Backend/Map4.java
@@ -16,9 +16,9 @@ public class Map4 extends Map{
         gameObjects.add(new Trap(new GamePosition(32, 3), 3));
         gameObjects.add(new Trap(new GamePosition(38, 3), 3));
         gameObjects.add(new Trap(new GamePosition(42, 3), 3));
-        gameObjects.add(new Trap(new GamePosition(47, 0), 3));
-        gameObjects.add(new Trap(new GamePosition(53, 0), 3));
-        gameObjects.add(new Trap(new GamePosition(56, 3), 3));
+        gameObjects.add(new Trap(new GamePosition(55, 0), 3));
+        gameObjects.add(new Trap(new GamePosition(58, 0), 3));
+        gameObjects.add(new Trap(new GamePosition(63, 3), 3));
         gameObjects.add(new Cliff(new GamePosition(65, 0), 10));
         gameObjects.add(new Land(68, 70,3.5 ,3));
         gameObjects.add(new Land(72, 74,3.5 ,3));
