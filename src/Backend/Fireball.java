ddaad96cc125dc454934ff07936e2497d813a353
caleb <calebshim248@gmail.com>
2023-05-12 09:40:52 -0400

    fireball faster

--- src/Backend/Fireball.java
+++ src/Backend/Fireball.java
@@ -2,7 +2,7 @@ package Backend;
 
 public class Fireball extends GameObject{
     Fireball(GamePosition position) {
-        super(position, 0.2, 0);
+        super(position, 0.5, 0);
     }
     public void move() {
         setPosition(new GamePosition(getPosition().getX() + getVelX(), getPosition().getY()));
