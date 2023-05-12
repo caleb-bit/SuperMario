5ea9a398be698ddda1df269b65099cac78376ef2
daowstin <austinzou2603@gmail.com>
2023-05-12 12:37:45 -0400

    restart added to game over

--- src/Backend/GameAPI.java
+++ src/Backend/GameAPI.java
@@ -59,6 +59,13 @@ public class GameAPI {
                 }
                 if (backend.getTimeLeft() <= 0) {
                     gameOver();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    restart();
+                    return;
                 }
                 if (getPlayer().getX() >= backend.getLevel(backend.getLevel()).getMap().getPoints().get(
                         backend.getLevel(backend.getLevel()).getMap().getPoints().size() - 1).getX()) {
